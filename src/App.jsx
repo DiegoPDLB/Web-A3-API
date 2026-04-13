7400c9037e683dad0403fc076b6b202ad2bd4a70
Diego Ponce de León Betanzos <111619317+DiegoPDLB@users.noreply.github.com>
2026-04-13 12:36:25 -0600

    13/04

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Login from "./views/Login";
 import Profile from "./views/Profile";
 import ResponsiveAppBar from "./components/AppBar";
+import { set } from "mongoose";
 
 const API_URL = "https//localhost:8000"
 
 function App() {
-    const isLogin = false
+    const [isLogin, setIsLogin] = useState(false)
+    const [user, setUser] = useState({})
     const login = async (user) => {
         const res = await fetch(API_URL+"/login", {
             method: "post",
@@ -13,6 +15,8 @@ function App() {
             body: JSON.stringify(user)
         })
         const data = await res.json()
+        setIsLogin(data.isLogin)
+        setUser(data.user)
         return data
     }
     return(
